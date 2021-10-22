ccd6325aa7512ed8f2a16449d8d2b0a68b27f679
Simon Moreau <simon@bim42.com>
2021-10-22 22:06:26 +0200

    set up the Hypar runner application

---
+++ HyparRunner/FunctionRunner.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Elements;
+using Hypar.Functions.Execution;
+using Hypar.Functions.Execution.AWS;
+using System.IO;
+
+namespace HyparRunner
+{
+    class FunctionRunner
+    {
+
+        public void RunFunction(string dllPath, string functionName)
+        {
+
+            Init();
+
+            Assembly DLL = Assembly.LoadFile(dllPath);
+
+            // Load dependencies
+            string dependenciesPath = Path.GetDirectoryName(dllPath) + "\\" + Path.GetFileNameWithoutExtension(dllPath) + ".Dependencies.dll";
+            if (File.Exists(dependenciesPath))
+            {
+                Assembly.LoadFile(dependenciesPath);
+            }
+
+            // string functionName = "EnvelopeByCenterline";
+            Type[] assemblyTypes = DLL.GetExportedTypes();
+
+            Type functionType = assemblyTypes.Where(t => t.Name == functionName).FirstOrDefault();
+            Type functionTypeOutputs = assemblyTypes.Where(t => t.Name == functionName + "Outputs").FirstOrDefault();
+            Type functionTypeInputs = assemblyTypes.Where(t => t.Name == functionName + "Inputs").FirstOrDefault();
+
+            if (functionType != null)
+            {
+                // var c = Activator.CreateInstance(functionType);
+                object inputs = Activator.CreateInstance(functionTypeInputs);
+
+                S3Args inputsBase = inputs as S3Args;
+
+                Dictionary<string, Model> inputModels = new Dictionary<string, Model>();
+
+                object outputs = functionType.InvokeMember(
+                    "Execute", BindingFlags.InvokeMethod | BindingFlags.Static | BindingFlags.Public,
+                 null, null, new object[] { inputModels, inputs });
+
+                ResultsBase outputsBase = outputs as ResultsBase;
+
+            }
+
+        }
+
+
+        Dictionary<string, Assembly> assemblies;
+
+        public void Init()
+        {
+
+            assemblies = new Dictionary<string, Assembly>();
+
+            AppDomain.CurrentDomain.AssemblyLoad += new AssemblyLoadEventHandler(CurrentDomain_AssemblyLoad);
+
+            AppDomain.CurrentDomain.AssemblyResolve += new ResolveEventHandler(CurrentDomain_AssemblyResolve);
+        }
+
+        Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+        {
+
+            Assembly assembly = null;
+
+            assemblies.TryGetValue(args.Name, out assembly);
+
+            return assembly;
+
+        }
+
+        void CurrentDomain_AssemblyLoad(object sender, AssemblyLoadEventArgs args)
+        {
+
+            Assembly assembly = args.LoadedAssembly;
+            assemblies[assembly.FullName] = assembly;
+        }
+    }
+}
---
+++ HyparRunner/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HyparRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = args[0];
+            FunctionRunner functionRunner = new FunctionRunner();
+            functionRunner.RunFunction(args[0], "EnvelopeByCenterline");
+
+        }
+    }
+}
